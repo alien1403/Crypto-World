f22e0862f23777695cf1cc636533d557760441a3
razvan1403 <hanghicelrazvanmihai@gmail.com>
2022-03-22 22:59:56 +0200

    Add CryptoAPI

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -2,14 +2,16 @@ var btc = document.getElementById("bitcoin");
 var eth = document.getElementById("ethereum");
 var doge = document.getElementById("dogecoin");
 
-var setting = {
+var liveprice = {
     "async": true,
-    "scrossDomain": true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%20ethereum%20dogecoin&vs_currencies=usd://api.coingecko.com/api/v3//price?ids=bitcoin%20ethereum%20dogecoin&vs_currencies=usd",
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
     "method": "GET",
     "headers": {}
-  }
+}
 
-  $.ajax(setting).done(function (response){
-    console.log(response)
-  });
+$.ajax(liveprice).done(function(response){
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+});
